fe4e8840f701605d79e85bab184a699ea0260e0d
BLWTF <badegbemile@gmail.com>
2022-06-25 16:08:46 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_login/app.dart';
+import 'package:user_repository/user_repository.dart';
+
+void main() {
+  BlocOverrides.runZoned(
+      () => runApp(
+            App(
+              authenticationRepository: AuthenticationRepository(),
+              userRepository: UserRepository(),
+            ),
+          ),
+      blocObserver: LoginObserver());
+}
+
+class LoginObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print('${bloc.runtimeType}: ${change}');
+  }
+}
